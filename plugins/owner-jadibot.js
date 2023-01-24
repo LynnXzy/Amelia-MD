46dd76ab8548c87a681ab978d898885971e89e55
CREATOR BOT WA <98082226+LynnXzy@users.noreply.github.com>
2023-01-24 20:07:37 +0800

    Update owner-jadibot.js

--- plugins/owner-jadibot.js
+++ plugins/owner-jadibot.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 //Plugin By Xynoz!!
 let handler = async (m, { conn, usedPrefix }) => {
 	let url = 'https://telegra.ph/file/f906fc225978ccaa06483.jpg'
-	conn.sendButton(m.chat, ' *❲ JADI BOT ❳*\n\n⎇ 14 Hari 13ᴋ\n⎇ 30 Hari 20ᴋ\n⎇ 40 Hari 27ᴋ\n⎇ 60 Hari 40ᴋ\n\n*ᴍɪɴᴀᴛ ᴄʜᴀᴛ ᴏᴡɴᴇʀ*', wm, await(await fetch(url)).buffer(), [['Owner',`/owner`]],m)
+	conn.sendButton(m.chat, ' *❲ JADI BOT ❳*\n\n⎇ 14 Hari Rm 13\n⎇ 30 Hari Rm 20\n⎇ 40 Hari 27ᴋ\n⎇ 60 Hari Rm 40\n\n*ᴍɪɴᴀᴛ ᴄʜᴀᴛ ᴏᴡɴᴇʀ*', wm, await(await fetch(url)).buffer(), [['Owner',`/owner`]],m)
 }
 handler.command = /^(jadibot2)$/i
 handler.tags = ['info']
