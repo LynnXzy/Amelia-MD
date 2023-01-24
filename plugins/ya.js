d14ba71ed07e28df4eb0ee383185658efd676fda
CREATOR BOT WA <98082226+LynnXzy@users.noreply.github.com>
2023-01-24 20:43:23 +0800

    Update ya.js

--- plugins/ya.js
+++ plugins/ya.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 //Plugin By Xynoz!!
 let handler = async (m, { conn, usedPrefix }) => {
-	let url = 'https://telegra.ph/file/ba72d2764080b10645d58.jpg'
+	let url = 'https://telegra.ph/file/c518a6fc95c4fd5d3672e.jpg'
 	conn.sendButton(m.chat, '*KIRIM BUKTI TF KE OWNER*', wm, await(await fetch(url)).buffer(), [['Owner',`/owner`]],m)
 }
 handler.command = /^(bayar|pembayaran)$/i
